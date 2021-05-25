5f2dba664817ae01eeaca0417d1d89e8124b6774
Vincent Richitelli <vin.richitelli@gmail.com>
2021-05-25 16:54:20 -0400

    more changes

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 // app.use(require("./routes/apiRoutes"));
 
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/public/index.html'));
+// })
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
